568b49217000f6bfe77c077e6d367fa035355d8e
Shockch4rge <favouriteteo@gmail.com>
2021-08-13 17:48:50 +0800

    Trying out BottomSheetBehaviour
    Took 53 seconds

--- app/src/main/java/com/example/bitjam/MainActivity.java
+++ app/src/main/java/com/example/bitjam/MainActivity.java
@@ -4,10 +4,12 @@ import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.inputmethod.InputMethodManager;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
@@ -18,35 +20,50 @@ import androidx.navigation.ui.NavigationUI;
 import com.example.bitjam.ViewModels.PlaylistViewModel;
 import com.example.bitjam.ViewModels.SongViewModel;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
 import com.google.firebase.firestore.Query;
 
 import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
-    BottomNavigationView bottomNav;
-    NavHostFragment navHostFragment;
-
-    @RequiresApi(api = Build.VERSION_CODES.R)
     @SuppressLint("NonConstantResourceId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_hub);
 
-        // Sets up the UI
-        Window window = this.getWindow();
-        View decorView = window.getDecorView();
-        int uiOptions =
-                View.SYSTEM_UI_FLAG_IMMERSIVE |
+        // Day UI
+        int uiOptions = View.SYSTEM_UI_FLAG_IMMERSIVE |
                 View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR |
                 View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR;
+        Window window = getWindow();
+        View decorView = window.getDecorView();
         window.setNavigationBarColor(getColor(R.color.white));
-        decorView.setSystemUiVisibility(uiOptions);
         window.setStatusBarColor(getColor(R.color.white));
+        decorView.setSystemUiVisibility(uiOptions);
+
+        BottomNavigationView bottomNav = findViewById(R.id.navBar);
+        View bottomSheet = findViewById(R.id.bottomSheet);
+
+        BottomSheetBehavior<View> mBottomSheet = BottomSheetBehavior.from(bottomSheet);
+
+        mBottomSheet.addBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
+            @Override
+            public void onStateChanged(@NonNull View bottomSheet, int newState) {
+
+            }
+
+            @Override
+            public void onSlide(@NonNull View bottomSheet, float slideOffset) {
+
+            }
+        });
+
+        bottomSheet.setOnClickListener(view ->
+                mBottomSheet.setState(BottomSheetBehavior.STATE_EXPANDED));
 
         // Fragment Navigation
-        bottomNav = findViewById(R.id.navBar);
-        navHostFragment = (NavHostFragment) getSupportFragmentManager()
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.fragment_container);
         assert navHostFragment != null;
         NavigationUI.setupWithNavController(bottomNav, navHostFragment.getNavController());
@@ -58,7 +75,7 @@ public class MainActivity extends AppCompatActivity {
      * @param v The current view
      */
     public static void hideKeyboardIn(View v) {
-        InputMethodManager imm = (InputMethodManager) v.getContext().getSystemService(Activity.INPUT_METHOD_SERVICE);
-        imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+        ((InputMethodManager) v.getContext().getSystemService(Activity.INPUT_METHOD_SERVICE))
+                .hideSoftInputFromWindow(v.getWindowToken(), 0);
     }
 }
